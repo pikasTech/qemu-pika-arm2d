1dea7843f05c55f115496e73e8d58560f29583ff
graduateDesign <graduateDesign@example.com>
2021-08-28 01:10:53 +0800

    update all

---
+++ applications/app-arm2d.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-08-28     lyon       the first version
+ */
+#ifndef APPLICATIONS_APP_ARM2D_H_
+#define APPLICATIONS_APP_ARM2D_H_
+
+void arm_2d_app_init(void);
+void arm_2d_app_update();
+
+#endif /* APPLICATIONS_APP_ARM2D_H_ */
---
+++ packages/pikascript/main.py
@@ -0,0 +1,8 @@
+import PikaStdLib
+
+print('hello world')
+mem = PikaStdLib.MemChecker()
+print('mem.max :')
+mem.max()
+print('mem.now :')
+mem.now()
