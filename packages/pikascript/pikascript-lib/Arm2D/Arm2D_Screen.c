7fcbe562b6c43c6a3a6a72481b432221a0c3052e
graduateDesign <graduateDesign@example.com>
2021-08-28 23:34:07 +0800

    drow sun is ok

--- packages/pikascript/pikascript-lib/Arm2D/Arm2D_Screen.c
+++ packages/pikascript/pikascript-lib/Arm2D/Arm2D_Screen.c
@@ -14,9 +14,25 @@ void Arm2D_Screen_init(PikaObj *self)
     obj_run(self, "background.init()");
 }
 
+extern const uint8_t c_bmpSun[56 * 57 * sizeof(uint16_t)];
+const static arm_2d_tile_t c_tPictureSun = {
+    .tRegion = {
+        .tSize = {
+            .iWidth = 56,
+            .iHeight = 57},
+    },
+    .tInfo.bIsRoot = true,
+    .phwBuffer = (uint16_t *)c_bmpSun,
+};
+
 void Arm2D_Screen_update(PikaObj *self, void *ptTile)
 {
     obj_setPtr(self, "ptTile", (void *)ptTile);
     obj_run(self, "background.update(ptTile)");
     obj_run(self, "elems.update(ptTile)");
+    arm_2d_rgb16_tile_copy_with_colour_masking(&c_tPictureSun,
+                                               ptTile,
+                                               NULL,
+                                               GLCD_COLOR_WHITE,
+                                               ARM_2D_CP_MODE_FILL);
 }
