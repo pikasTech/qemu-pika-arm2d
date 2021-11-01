a38d6635b1a746968230e8f03871d8de6c9eafbb
李昂 <liang6516@outlook.com>
2021-11-01 20:13:32 +0800

    box is ok

--- packages/pikascript/main.py
+++ packages/pikascript/main.py
@@ -7,19 +7,8 @@ screen = Arm2D.Screen()
 screen.init()
 screen.background.setColor('white')
 
-screen.elems.s1 = Arm2D.Star()
-screen.elems.s2 = Arm2D.Star()
-screen.elems.s3 = Arm2D.Star()
-
-screen.elems.s1.init()
-screen.elems.s2.init()
-screen.elems.s3.init()
-
-screen.elems.s1.move(100, 300)
-screen.elems.s1.speed(0.1)
-
-screen.elems.s2.centra(100, 100)
-screen.elems.s2.speed(0.5)
+screen.elems.b1 = Arm2D.Box()
+screen.elems.b1.init()
 
 print('hello world')
 print('mem used max:')
